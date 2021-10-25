67d4aff5a3ca2b4a62a776469df93e623c9e188f
nakatsinho <nakatsinho@gmail.com>
2021-10-25 07:06:39 +0200

    Resource file

---
+++ app/src/main/java/com/keltoncumbe/challenge/witweather_android/utils/Resource.kt
@@ -0,0 +1,18 @@
+package com.keltoncumbe.challenge.witweather_android.utils
+
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+    companion object {
+        fun <T> success(data: T): Resource<T> = Resource(Status.SUCCESS, data, null)
+
+        fun <T> error(data: T?, message: String?): Resource<T> =
+            Resource(Status.ERROR, data, message)
+
+        fun <T> loading(data: T?): Resource<T> = Resource(Status.LOADING, data, null)
+    }
+}
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
