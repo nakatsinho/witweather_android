cc54fccc94f659e1d93b3e24d270aae12dcc841d
nakatsinho <nakatsinho@gmail.com>
2021-10-25 06:59:42 +0200

    New: Create const file for Api key replacement

---
+++ app/src/main/java/com/keltoncumbe/challenge/witweather_android/utils/enviroment/API_KEY.kt
@@ -0,0 +1,5 @@
+package com.keltoncumbe.challenge.witweather_android.utils.enviroment
+
+const val URL_PATH = "https://api.openweathermap.org/data/2.5/"
+const val API_KEY = "2f26f4026a31178c80d27a7b501e15a9"
+
