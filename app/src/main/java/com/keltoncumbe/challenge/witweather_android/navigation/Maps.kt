2d524fb96f9f833aea8f5eea03921e5ee526dcc0
nakatsinho <nakatsinho@gmail.com>
2021-10-25 06:58:53 +0200

    New: Creating Navigation route files

--- app/src/main/java/com/keltoncumbe/challenge/witweather_android/navigation/Maps.kt
+++ app/src/main/java/com/keltoncumbe/challenge/witweather_android/navigation/Maps.kt
@@ -1,6 +1,7 @@
 package com.keltoncumbe.challenge.witweather_android.navigation
 
 sealed class Maps( val  route: String){
+    object  Splash:Maps("splash_screen")
     object  AllCitiesList : Maps("all_cities_list")
-//    object  DetailScreen : Maps("city_details")
+    object  DetailScreen : Maps("city_details")
 }
--- app/src/main/java/com/keltoncumbe/challenge/witweather_android/navigation/Routes.kt
+++ app/src/main/java/com/keltoncumbe/challenge/witweather_android/navigation/Routes.kt
@@ -4,17 +4,29 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.keltoncumbe.challenge.witweather_android.views.AllCitiesList
+import com.keltoncumbe.challenge.witweather_android.views.Splash
+import com.keltoncumbe.challenge.witweather_android.views.cities.AllCitiesList
+import com.keltoncumbe.challenge.witweather_android.views.cities.CityDetails
 
 @Composable
 fun Routes(){
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = Maps.AllCitiesList.route)
+    NavHost(navController = navController, startDestination = Maps.Splash.route)
     {
+        composable(route = Maps.Splash.route)
+        {
+            Splash(navController)
+        }
+
         composable(route = Maps.AllCitiesList.route)
         {
             AllCitiesList(navController)
+        }
+
+        composable(route = Maps.DetailScreen.route)
+        {
+            CityDetails(navController)
         }
     }
 }
