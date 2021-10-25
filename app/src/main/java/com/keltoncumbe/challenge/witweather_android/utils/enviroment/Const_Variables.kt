b3db5a11f81841b957a8858fd19de59e4b800c7d
nakatsinho <nakatsinho@gmail.com>
2021-10-25 07:00:25 +0200

    Adding extra Const variables

---
+++ app/src/main/java/com/keltoncumbe/challenge/witweather_android/utils/enviroment/Const_Variables.kt
@@ -0,0 +1,21 @@
+package com.keltoncumbe.challenge.witweather_android.utils.enviroment
+
+const val INTENT_WEATHER_RESULT = "weather_result"
+
+const val UNITS_METRIC = "metric"
+
+const val LISBON = "2267057"
+const val MADRID = "3117735"
+const val MOSCOW = "524901"
+const val PARIS = "2988507"
+const val BERLIN = "2950159"
+const val COPENHAGEN = "2618425"
+const val ROME = "5134295"
+const val LONDON = "2643743"
+const val DUBLIN = "5344157"
+const val PRAGUE = "3067696"
+const val VIENNA = "2761369"
+
+const val MY_CITY_NAME = "Maputo"
+
+const val EUROPEAN_CITIES = "$LISBON,$MADRID,$MOSCOW,$PARIS,$BERLIN,$COPENHAGEN,$ROME,$LONDON,$DUBLIN,$PRAGUE,$VIENNA"
